fe42921fbddb93e1a6dd49a8e2b380af207f6f92
VegardIversen <vegard-si@hotmail.com>
2022-10-23 20:03:01 +0200

    bug fix for swerling 2

--- exercise.py
+++ exercise.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from rftool.radar import Albersheim, Shnidman
 from scipy import constants
-
+import decimal
+decimal.getcontext().prec = 100
 
 Xband_freq = 9400e6
 horisontal_beam_width = 0.8 #degrees
@@ -25,6 +26,7 @@ roc = 10 #ROC in meters
 total_loss = transmitter_loss + receiver_loss + signal_processing_loss #dB
 duty_cycle = pulse_width*pulse_repetition_frequency
 p_d = np.arange(0.1, 0.99, 0.01)
+n_p_td = (constants.c/Xband_freq)//T_d #number of pulses per dwell time
 
 #function to convert from dB to linear
 def db2lin(x):
@@ -41,17 +43,49 @@ def lin2db_amp(x):
 #with swerling0
 
 
-def swerling0(p_fa, peak_power, pulse_width, pulse_repetition_frequency, receiver_noise_figure, receiver_bandwidth, transmitter_loss, receiver_loss, signal_processing_loss):
-    return 10*np.log10(peak_power*pulse_width*pulse_repetition_frequency*(10**(receiver_noise_figure/10))/(receiver_bandwidth*(10**(transmitter_loss/10))*(10**(receiver_loss/10))*(10**(signal_processing_loss/10))*p_fa))
+
+
+def Shindman_equation(p_d, p_fa, swerling, N):
+    C = 0
+    alpha = 0
+
+    if N > 40:
+        alpha = np.divide(1,4)
+    #print(p_fa)
+
+    eta = np.sqrt(-0.8*np.log(4*p_fa*(1-p_fa))) + np.sign(p_d - 0.5)*(np.sqrt(-0.8*np.log(4*p_d*(1-p_d))))
+   
+    X_inf = eta * (eta + 2*np.sqrt(N/2 + (alpha - 1/4)))
+    
+    def K(SW):
+        if SW == 1:
+            return 1
+        elif SW == 2:
+            return N
+        elif SW == 3:
+            return 2
+        elif SW == 4:
+            return 2*N
+
+    if swerling == 0:
+        C = 1
+    else:
+        C1 = (((17.7006*p_d - 18.4496)*p_d + 14.5339)*p_d - 3.525)/K(swerling)
+        
+        C2 = 1/K(swerling) * (np.exp(27.31*p_d - 25.14) + (p_d-0.8)*(0.7*np.log((1e-5)/p_fa) + (2*N -  20)/80))
+        C_db = 0
+
+        if p_d <= 0.872 and p_d >= 0.1:
+            C_db = C1
+        elif p_d > 0.872 and p_d <= 0.99:
+            C_db = C1 + C2
+        C = np.power(10, C_db/10)
+    X1 = np.divide(C*X_inf, N)
+    SNR_dB = 10*np.log10(X1)
+    return SNR_dB
 
 
 
-#plot the result
-def plot_result(p_fa, peak_power, pulse_width, pulse_repetition_frequency, receiver_noise_figure, receiver_bandwidth, transmitter_loss, receiver_loss, signal_processing_loss):
-    range = np.arange(0, 10000, 1)
-    p_d = swerling0(p_fa, peak_power, pulse_width, pulse_repetition_frequency, receiver_noise_figure, receiver_bandwidth, transmitter_loss, receiver_loss, signal_processing_loss)
-    plt.plot(range, p_d)
-    plt.show()
 
 def range_det(Pt, Gt,Gr, lam, roc, n_p, SNR, F, B, Ls, T0=290):
     return np.power((Pt*Gt*Gr*np.power(lam,2)*roc*n_p)/(np.power(4*np.pi, 3) * SNR*constants.Boltzmann *T0*F*B*Ls),1/4)
@@ -62,27 +96,33 @@ def range_det(Pt, Gt,Gr, lam, roc, n_p, SNR, F, B, Ls, T0=290):
 def Albersheim_arr(p_d_arr, p_fa=1e-6, N=1):
     return Albersheim(p_fa, p_d_arr, N)
 
-def Shnidman_arr(p_d_arr, swir, p_fa=1e-6, N=1):
+def Shnidman_arr(p_d_arr, swir, p_fa=1e-6, N=1): #only works for swerling 0
+
     return Shnidman(p_fa, p_d_arr, N, swir)
 
+def Shnidman_arr2(p_d_arr, swir, p_fa=1e-6, N=1):
+    arr = np.zeros(len(p_d_arr))
+    for i in range(len(p_d_arr)):
+        arr[i] = Shindman_equation(p_d_arr[i], p_fa, swir, N)
+    return arr
 if __name__ == "__main__":
     R = np.arange(10, 10000, 1)
-    snr_arr_alb = Albersheim_arr(p_d, p_fa, n_p)
-    snr_arr_shn = Shnidman_arr(p_d, 0, p_fa, n_p)
-
-    #plt.plot(lin2db(snr_arr_shn), p_d, label='Shnidman')
-
-    #print(snr_arr_alb)
-    #SNR_Alb = Albersheim(p_fa, 0.9, 1)
-    #SNR_Shn = Shnidman(p_fa, 0.9, 1, 0)
+    snr_arr_alb = Albersheim_arr(p_d, p_fa, n_p_td)
+    snr_arr_shn = Shnidman_arr(p_d, 0, p_fa, n_p_td)
+    snr_arr_shn1 = Shnidman_arr2(p_d, 1, p_fa, n_p_td)
+    
+   
+    
     range_det_alb_arr = range_det(peak_power, db2lin(antenna_gain), db2lin(antenna_gain), constants.c/Xband_freq, roc, n_p, db2lin(snr_arr_alb), db2lin(receiver_noise_figure),receiver_bandwidth, db2lin(total_loss))
     range_det_shn_arr = range_det(peak_power, db2lin(antenna_gain), db2lin(antenna_gain), constants.c/Xband_freq, roc, n_p, db2lin(snr_arr_shn), db2lin(receiver_noise_figure),receiver_bandwidth, db2lin(total_loss))
+    range_det_shn1_arr = range_det(peak_power, db2lin(antenna_gain), db2lin(antenna_gain), constants.c/Xband_freq, roc, n_p, db2lin(snr_arr_shn1), db2lin(receiver_noise_figure),receiver_bandwidth, db2lin(total_loss))
     #SNR_arr = SNR_array(peak_power, db2lin(antenna_gain), db2lin(antenna_gain), constants.c/Xband_freq, sigma, 1, db2lin(receiver_noise_figure), receiver_bandwidth, db2lin(total_loss))
-    #plt.plot(lin2db(snr_arr_alb), p_d, label='Albersheim')
     plt.plot(range_det_alb_arr, p_d, label='Albersheim')
-    plt.plot(range_det_shn_arr, p_d, label='Shnidman')
+    plt.plot(range_det_shn_arr, p_d, label='Shnidman swerling 0')
+    plt.plot(range_det_shn1_arr, p_d, label='Shnidman swerling 1')
     plt.xlabel('Range [m]')
     plt.ylabel('p_d')
+    plt.title(f'Range vs p_d with swerling 0, 1 and Albersheim, with N={n_p_td} (coherently integrated pulses)') 
     plt.legend()
     plt.show()
     #print(np.power(constants.c/Xband_freq, 2))
